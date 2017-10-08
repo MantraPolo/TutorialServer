e43bb16cd883d51c2961c1badf45b38b841c8a3c
Marco <marcorlee@gmail.com>
2017-10-08 19:44:01 -0400

    Add project files.

---
+++ TutorialServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TutorialServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
